7d6b17a66f94263db898e1d9d749b0509bee304c
Lukáš Slivka <lukas2slivka@gmail.com>
2022-10-17 23:18:09 +0100

    ready to go

--- DetailAndGo/Models/Customer.cs
+++ DetailAndGo/Models/Customer.cs
@@ -6,5 +6,12 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime Registered { get; set; }
+        public string Email { get; set; }
+        public string PostCode { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string Notes { get; set; }
+
     }
 }
