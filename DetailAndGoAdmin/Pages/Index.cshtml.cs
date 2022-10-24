7aec72ab0a9a7da2c02d79aecd7111f732d9d0fb
Lukas Slivka <lukas2slivka@gmail.com>
2022-10-25 00:04:11 +0100

    some misc stuff done

--- DetailAndGoAdmin/Pages/Index.cshtml.cs
+++ DetailAndGoAdmin/Pages/Index.cshtml.cs
@@ -8,6 +8,7 @@ namespace DetailAndGoAdmin.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Utility _utility = new Utility();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -16,7 +17,10 @@ namespace DetailAndGoAdmin.Pages
 
         public void OnGet()
         {
-         
+            ViewData["Greeting"] = _utility.GetGreetings();
+            string[] quote = _utility.GetRandomQuote().Result;
+            ViewData["Quote"] = quote[0];
+            ViewData["QuoteAuthor"] = quote[1];
         }
     }
 }
---
+++ DetailAndGoAdmin/Utility.cs
@@ -0,0 +1,34 @@
+﻿namespace DetailAndGoAdmin
+{
+    public class Utility
+    {
+        public string GetGreetings()
+        {
+            if(DateTime.Now.Hour > 0 && DateTime.Now.Hour < 6)
+            {
+                return "Night night!";
+            }
+            else if(DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+            {
+                return "Good morning!";
+            }
+            else if(DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 17)
+            {
+                return "Good afternoon!";
+            }
+            else
+            {
+                return "Good evening!";
+            }
+        }
+
+        public async Task<string[]> GetRandomQuote()
+        {
+            Random random = new Random();
+            string[] quotes = await File.ReadAllLinesAsync("Quotes.txt");            
+            string selectedQuote = quotes[random.Next(0, quotes.Length - 1)];
+            string[] result = selectedQuote.Split(';');
+            return result;
+        }
+    }
+}
