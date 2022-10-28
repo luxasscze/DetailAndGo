3eede5c30fd139257ae7983a7ecdd4c411c1411d
Lukas Slivka <lukas2slivka@gmail.com>
2022-10-28 10:25:05 +0100

    bleh

--- DetailAndGoAdmin/Pages/Index.cshtml.cs
+++ DetailAndGoAdmin/Pages/Index.cshtml.cs
@@ -1,29 +1,38 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DetailAndGo.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DetailAndGoAdmin.Pages
 {
+    
+
     [Authorize(Roles = "Admin")]
     public class IndexModel : PageModel
     {
+        private readonly DetailAndGoAdmin.Data.ApplicationDbContext _context;
         private readonly ILogger<IndexModel> _logger;
         private readonly Utility _utility = new Utility();
-        private IWebHostEnvironment _webHostEnvironment;
+        private IWebHostEnvironment _webHostEnvironment;        
 
-        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment webHostEnvironment)
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment webHostEnvironment, DetailAndGoAdmin.Data.ApplicationDbContext context)
         {
             _logger = logger;
             _webHostEnvironment = webHostEnvironment;
+            _context = context;
         }
 
-        
+        public IList<Job> Jobs { get; set; }
 
 
-        public void OnGet()
-        {            
+        public async Task OnGetAsync()
+        {
+            if (_context.Jobs != null)
+            {
+                Jobs = _context.Jobs.ToList();
+            }
             ViewData["Greeting"] = _utility.GetGreetings();
-            string[] quote = _utility.GetRandomQuote(_webHostEnvironment.WebRootPath).Result;
+            string[] quote = await _utility.GetRandomQuote(_webHostEnvironment.WebRootPath);
             ViewData["Quote"] = quote[0];
             ViewData["QuoteAuthor"] = quote[1];
         }
