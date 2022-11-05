ee06432993a1d8babfbc18615913a54e9b9b191f
Lukas Slivka <lukas2slivka@gmail.com>
2022-11-05 12:52:28 +0000

    registration nearly working

---
+++ DetailAndGo/Migrations/20221105114033_PhoneAdded.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DetailAndGo.Migrations
+{
+    public partial class PhoneAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Customers");
+        }
+    }
+}
