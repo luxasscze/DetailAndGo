20122c265cea6fe136eb778ab7b2adacd2ac9c0a
Lukas Slivka <lukas2slivka@gmail.com>
2023-01-17 00:11:38 +0000

    car manufacture year added

---
+++ DetailAndGo/Migrations/20230116231002_CarManufactureYear.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DetailAndGo.Migrations
+{
+    public partial class CarManufactureYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ManufactureYear",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManufactureYear",
+                table: "Cars");
+        }
+    }
+}
