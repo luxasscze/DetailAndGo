408f88bb494dd7bfb81f5b280734ede60b9d8dcd
Lukas Slivka <lukas2slivka@gmail.com>
2023-05-22 12:47:25 +0100

    services in admin still in progress

--- DetailAndGoAdmin/Pages/Jobs/JobDetail.cshtml.cs
+++ DetailAndGoAdmin/Pages/Jobs/JobDetail.cshtml.cs
@@ -29,7 +29,7 @@ namespace DetailAndGoAdmin.Pages.Jobs
             car = await _carService.GetCarById(booking.CarId);
             services = await GetServicesFromArray(booking.ServicesArray);
             subServices = await _serviceService.GetServicesFromIdArray(booking.SubServicesArray);
-            subServices = subServices.Where((_, i) => i % 2 != 0).ToList();
+            subServices = subServices.Where((number, index) => index % 2 == 1).ToList();
 
         }
 
